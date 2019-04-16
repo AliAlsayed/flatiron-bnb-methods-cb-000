e3736ace9693b4a07e796aa1dea0d1ba66f994cd
Ali Alsayed <a_alsayed87@hotmail.com>
2019-04-16 06:49:11 +0000

    Automatically backed up by Learn

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,4 +1,21 @@
 class Neighborhood < ActiveRecord::Base
   belongs_to :city
   has_many :listings
+
+  def neighborhood_openings(check_in, check_out)
+    openings = []
+
+    listings.each do |l|
+      available = true
+
+      l.reservations do |r|
+        booking_days = r.checkin..r.checkout
+        if booking_days.include?(check_in) || if booking_days.include?(check_out)
+          available = false
+          break
+        end
+      end
+      openings << l if available
+    end
+  end
 end
