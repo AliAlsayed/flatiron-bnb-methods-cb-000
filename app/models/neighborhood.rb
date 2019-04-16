ab7f64ea4e8abc24771297f40b54597b1b77a264
Ali Alsayed <a_alsayed87@hotmail.com>
2019-04-16 07:48:21 +0000

    Automatically backed up by Learn

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -34,17 +34,16 @@ class Neighborhood < ActiveRecord::Base
   end
 
   def self.most_res
-    # max_res = 0
-    # most_res_n = ''
-    # self.all.each do |n|
-    #   res_count = n.reservations.length
-    #   if res_count > max_res
-    #     max_res = res_count
-    #     most_res_n = n
-    #   end
-    # end
-    # most_res_n
-    Neighborhood.all.max_by{|n| n.reservations}
+    max_res = 0
+    most_res_n = ''
+    self.all.each do |n|
+      res_count = n.reservations.length
+      if res_count > max_res
+        max_res = res_count
+        most_res_n = n
+      end
+    end
+    most_res_n
   end
 
 end
