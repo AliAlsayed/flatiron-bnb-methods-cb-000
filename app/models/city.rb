60c00107f1e3747cbc6d2db47136ac0a0fed10bd
Ali Alsayed <a_alsayed87@hotmail.com>
2019-04-16 07:52:15 +0000

    Automatically backed up by Learn

--- app/models/city.rb
+++ app/models/city.rb
@@ -3,9 +3,17 @@ class City < ActiveRecord::Base
   has_many :listings, :through => :neighborhoods
 
   def city_openings(check_in, check_out)
-    listings.select do |listing|
-      listing.reservations.last#.checkout < check_in.to_date
+    openings = []
+    listings.each do |l|
+      available = true
+      l.reservations.each do |r|
+        booking_days = r.checkin..r.checkout
+        if booking_days.include?(check_in.to_date) || booking_days.include?(check_out.to_date)
+          available = false
+        end
+      end
+      openings << l if available
     end
+    openings
   end
-
 end
