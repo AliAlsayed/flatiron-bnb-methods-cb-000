63b35692917048cd530deee90ef27aeb74ca0ae9
Ali Alsayed <a_alsayed87@hotmail.com>
2019-04-16 06:28:39 +0000

    Automatically backed up by Learn

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,10 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
-end
+  def city_openings(check_in, check_out)
+    listings.select do |listing|
+      listing.reservations.last#.checkout < check_in.to_date
+    end
+  end
 
+end
