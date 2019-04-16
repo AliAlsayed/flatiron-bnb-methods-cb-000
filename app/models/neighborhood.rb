f87e27b6520494e7205e40ca70b1f0fc1e1f1f1b
Ali Alsayed <a_alsayed87@hotmail.com>
2019-04-16 07:18:40 +0000

    Automatically backed up by Learn

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -3,18 +3,17 @@ class Neighborhood < ActiveRecord::Base
   has_many :listings
 
   def neighborhood_openings(check_in, check_out)
-    listings.merge(Listing.available(check_in, check_out))
-    # openings = []
-    # listings.each do |l|
-    #   available = true
-    #   l.reservations.each do |r|
-    #     booking_days = r.checkin..r.checkout
-    #     if booking_days.include?(check_in.to_date) || booking_days.include?(check_out.to_date)
-    #       available = false
-    #     end
-    #   end
-    #   openings << l if available
-    # end
-    # openings
+    openings = []
+    listings.each do |l|
+      available = true
+      l.reservations.each do |r|
+        booking_days = r.checkin..r.checkout
+        if booking_days.include?(check_in.to_date) || booking_days.include?(check_out.to_date)
+          available = false
+        end
+      end
+      openings << l if available
+    end
+    openings
   end
 end
